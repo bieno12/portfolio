c33de489cad7261ad87be272a697174feea80658
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-01-31 14:30:29 +0200

    add hero section to app with data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
+import { HeroSection } from "./hero-section/hero-section";
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, HeroSection],
   templateUrl: './app.html',
 })
 export class App {
